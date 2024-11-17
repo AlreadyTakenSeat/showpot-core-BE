ea554d0a908e46b739627af95c6642f4f0ebcee6
GaBaljaintheroom <junsu1222@naver.com>
2024-11-18 00:31:44 +0900

    refactor: RestClientConfig 의미 없는 어노테이션 삭제

--- app/api/common-api/src/main/java/org/example/config/RestClientConfig.java
+++ app/api/common-api/src/main/java/org/example/config/RestClientConfig.java
@@ -1,13 +1,11 @@
 package org.example.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClient.Builder;
 
 @Configuration
-@RequiredArgsConstructor
 public class RestClientConfig {
 
     @Bean
