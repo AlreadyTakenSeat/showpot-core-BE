d0bbc92997fe8ea31dd7abac92b2b41c4f029600
GaBaljaintheroom <junsu1222@naver.com>
2024-12-23 14:41:46 +0900

    fix: 공연 알림 날짜 형식 수정

--- app/api/show-api/src/main/java/com/example/show/controller/dto/request/TicketingAlertReservationApiRequest.java
+++ app/api/show-api/src/main/java/com/example/show/controller/dto/request/TicketingAlertReservationApiRequest.java
@@ -2,16 +2,19 @@ package com.example.show.controller.dto.request;
 
 import com.example.show.controller.vo.TicketingApiType;
 import com.example.show.service.dto.request.TicketingAlertReservationServiceRequest;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import org.example.util.DateTimeUtil;
 import org.example.util.ValidateStatus;
 
 public record TicketingAlertReservationApiRequest(
 
-    @Schema(description = "공연 티켓팅 알림 시간 선택", example = "yyyy-MM-dd'T'HH:mm")
-    List<LocalDateTime> alertTimes
+    @ArraySchema(schema = @Schema(description = "공연 티켓팅 알림 시간", example = "2999-01-01T23:59"))
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    List<String> alertTimes
 ) {
 
     public TicketingAlertReservationServiceRequest toServiceRequest(
@@ -23,7 +26,9 @@ public record TicketingAlertReservationApiRequest(
             .userId(userId)
             .showId(showId)
             .type(type)
-            .alertAts(ValidateStatus.checkNullOrEmpty(alertTimes))
+            .alertAts(ValidateStatus.checkNullOrEmpty(alertTimes).stream()
+                .map(DateTimeUtil::parseDateTime)
+                .toList())
             .build();
     }
 }
