96608d0475df72fe4800429c659bb3723a2f8885
김승진 <ohksj77@gmail.com>
2025-01-01 16:59:34 +0900

    fix: 빈 리스트 검증 어노테이션 제거

--- app/api/show-api/src/main/java/com/example/show/controller/dto/request/TicketingAlertReservationApiRequest.java
+++ app/api/show-api/src/main/java/com/example/show/controller/dto/request/TicketingAlertReservationApiRequest.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
@@ -15,7 +14,6 @@ import org.example.util.ValidateStatus;
 
 public record TicketingAlertReservationApiRequest(
 
-    @NotEmpty
     @NotNull
     @ArraySchema(schema = @Schema(description = "공연 티켓팅 알림 시간", example = "2999-1-1 23:59"))
     @JsonFormat(pattern = "yyyy-M-dd HH:mm")
